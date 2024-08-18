2265fad513821a9b853034eee9c38488e60788b3
root <root@alpine-dev..>
2024-08-18 18:12:13 +0000

    feat: Initializing Stock and Comment models, create migration

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace APIStock.Models
+namespace StockAPI.Models
 {
     public class Comment
     {
--- Models/Stock.cs
+++ Models/Stock.cs
@@ -1,18 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace APIStock.Model
+namespace StockAPI.Models
 {
     public class Stock
     {
         public int Id { get; set; }
         public string Symbol { get; set; } = string.Empty;
         public string Company { get; set; } = string.Empty;
-        [column(TypeName = "decimal(18,2)")]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Purchase{ get; set; }
-        [column(TypeName = "decimal(18,2)")]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal LastDiv { get; set; }
         public long MarketCap { get; set; }
         public List<Comment> Comments { get; set; } = new List<Comment>();
