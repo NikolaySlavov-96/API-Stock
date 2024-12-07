742f26bcada8ba01586d2fa06a87d0ef09f1bd62
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-12-07 20:44:00 +0000

    feat: Implement NewUserDto and integrate with registration process

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using StockAPI.Dtos.Account;
+using StockAPI.Interfaces;
 using StockAPI.Models;
 
 namespace StockAPI.Controllers
@@ -14,9 +15,11 @@ namespace StockAPI.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<AppUser> _userManager;
-        public AccountController(UserManager<AppUser> userManager)
+        private readonly ITokenService _tokenService;
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
         {
             _userManager = userManager;
+            _tokenService = tokenService;
         }
 
         [HttpPost("register")]
@@ -41,7 +44,14 @@ namespace StockAPI.Controllers
                     var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
                     if (roleResult.Succeeded)
                     {
-                        return Ok("User created");
+                        return Ok(
+                            new NewUserDto
+                            { 
+                                UserName = appUser.UserName,
+                                Email = appUser.Email,
+                                Token = _tokenService.CreationToken(appUser),
+                            }
+                        );
                     }
                     else
                     {
